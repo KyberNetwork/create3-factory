d60fa8fc7ab1539f7387491d7b599b4a8929e38a
Cuong Tran <tranquoccuong@outlook.com.vn>
2025-06-30 18:37:01 +0700

    feat: update codebase

--- README.md
+++ README.md
@@ -11,28 +11,6 @@ One could use a `CREATE2` factory that deterministically deploys contracts to an
 
 A `CREATE3` factory offers the best solution: the address of the deployed contract is determined by only the deployer address and the salt. This makes it far easier to deploy contracts to multiple chains at the same addresses.
 
-## Deployments
-
-`CREATE3Factory` has been deployed to `0x9fBB3DF7C40Da2e5A0dE984fFE2CCB7C47cd0ABf` on the following networks:
-
-- Ethereum Mainnet
-- Ethereum Goerli Testnet
-- Ethereum Sepolia Testnet
-- Ethereum Holesky Testnet
-- Arbitrum Mainnet
-- Arbitrum Goerli Testnet
-- Avalanche C-Chain Mainnet
-- Fantom Opera Mainnet
-- Optimism Mainnet
-- Polygon Mainnet
-- Gnosis Chain Mainnet
-- BSC Mainnet
-- Base Mainnet
-- Base Sepolia Testnet
-- Blast Mainnet
-- Sanko Mainnet
-- Unichain Mainnet
-
 ## Usage
 
 Call `CREATE3Factory::deploy()` to deploy a contract and `CREATE3Factory::getDeployed()` to predict the deployment address, it's as simple as it gets.
@@ -71,5 +49,5 @@ forge build
 Make sure that the network is defined in foundry.toml, then run:
 
 ```bash
-./deploy/deploy.sh [network]
+forge script script/Deploy.s.sol
 ```
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,17 +2,33 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "forge-std/StdJson.sol";
 
 import {CREATE3Factory} from "../src/CREATE3Factory.sol";
 
 contract DeployScript is Script {
+    using stdJson for string;
+
     function run() public returns (CREATE3Factory factory) {
-        uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
-
-        vm.startBroadcast(deployerPrivateKey);
-
-        factory = new CREATE3Factory();
-
+        vm.startBroadcast();
+        factory = new CREATE3Factory{salt: "CREATE3Factory"}();
+        _writeAddress("deployments/create3Factory.json", block.chainid, address(factory));
         vm.stopBroadcast();
+    }
+
+    function _writeAddress(string memory path, uint256 chainId, address value) internal {
+        if (!vm.isContext(VmSafe.ForgeContext.ScriptBroadcast)) {
+            return;
+        }
+        vm.serializeJson(path, _getJsonString(path));
+        vm.writeJson(path.serialize(vm.toString(chainId), value), path);
+    }
+
+    function _getJsonString(string memory path) internal view returns (string memory) {
+        try vm.readFile(path) returns (string memory json) {
+            return json;
+        } catch {
+            return "{}";
+        }
     }
 }
